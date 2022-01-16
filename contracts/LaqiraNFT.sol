824bb259b946e2a94e2c6d44325c02b04f6ca90b
lightY <lighty8214@gmail.com>
2022-01-16 21:39:34 +0330

    Add LaqiraNFT contract

--- contracts/LaqiraNFT.sol
+++ contracts/LaqiraNFT.sol
@@ -3,8 +3,39 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract LaqiraNFT {
-    constructor() {
-        // NFT marketplace
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract LaqiraNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    uint256 private NFTCounter;
+    uint256 private mintingFee;
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return "ipfs://";
+    }
+
+    function mint(string memory _tokenURI) public payable {
+        uint256 transfferdAmount = msg.value;
+        
+        require(transfferdAmount >= mintingFee, '');
+
+        _tokenIds.increment();
+        
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(_msgSender(), newTokenId);
+
+        _setTokenURI(newTokenId, _tokenURI);
+    }
+
+    function setMintingFeeAmount(uint256 _amount) public onlyOwner {
+        mintingFee = _amount;       
     }
 }
