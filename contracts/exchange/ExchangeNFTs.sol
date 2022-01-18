2585d40aff5d293d88a20173dc439fc7a9c39a26
lightY <lighty8214@gmail.com>
2022-01-18 21:33:22 +0330

    Required contracts were imported

---
+++ contracts/exchange/ExchangeNFTs.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import '@openzeppelin/contracts/utils/math/Math.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
+
+contract ExchangeNFTs {
+
+}
