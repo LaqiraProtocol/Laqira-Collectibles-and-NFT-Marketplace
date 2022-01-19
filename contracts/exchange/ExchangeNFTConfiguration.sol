3b3b2cf58e4881e5f0fe5fd58541ffc8f340a038
lightY <lighty8214@gmail.com>
2022-01-19 03:43:32 +0330

    Required contracts were imported

--- contracts/exchange/ExchangeNFTConfiguration.sol
+++ contracts/exchange/ExchangeNFTConfiguration.sol
@@ -2,6 +2,10 @@
 
 pragma solidity ^0.8.0;
 
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
+import './interfaces/IExchangeNFTConfiguration.sol';
+
 contract ExchangeNFTConfiguration {
 
 }
