c8a96e74c9c5d09cc2a14d6b7d577b1b6d5841a8
lightY <lighty8214@gmail.com>
2022-03-06 03:20:47 +0330

    Add upgradeable package

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('@openzeppelin/hardhat-upgrades');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
